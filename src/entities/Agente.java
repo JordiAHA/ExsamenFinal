253b19aa45b05e7b579d69a73f5a7e19c3e0e15f
JordiAHA <jahernandeza@ufpso.edu.co>
2024-12-13 07:55:03 -0500

    exsamen

---
+++ src/entities/Agente.java
@@ -0,0 +1,48 @@
+package entities;
+
+public class Agente extends Entidad {
+    private String habilidadEspecial;
+    private String misionAsignada;
+    private int nivel; 
+
+    public Agente(int id, String nombre, String habilidadEspecial) {
+        super(id, nombre);
+        this.habilidadEspecial = habilidadEspecial;
+        this.nivel = 0; 
+    }
+
+    public String getHabilidadEspecial() {
+        return habilidadEspecial;
+    }
+
+    public void setHabilidadEspecial(String habilidadEspecial) {
+        this.habilidadEspecial = habilidadEspecial;
+    }
+
+    public String getMisionAsignada() {
+        return misionAsignada;
+    }
+
+    public void asignarMision(String mision) {
+        this.misionAsignada = mision;
+    }
+
+    
+    public void incrementarNivel() {
+        this.nivel++;
+    }
+
+    public int getNivel() {
+        return nivel;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + ", Habilidad Especial: " + habilidadEspecial 
+                + (misionAsignada != null ? ", Misión: " + misionAsignada : "") 
+                + ", Nivel: " + nivel; 
+    }
+}
+
+
+
